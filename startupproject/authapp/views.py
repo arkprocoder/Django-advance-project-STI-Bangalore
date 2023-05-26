e73c474e1069e76bbf789296bfe353af4741163d
anees <aneesurrehman423@gmail.com>
2023-05-26 09:01:46 +0530

    day-3

--- startupproject/authapp/views.py
+++ startupproject/authapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signup(request):
+    return render(request,"signup.html")
+
+def handleLogin(request):
+    return render(request,"login.html")
+
+def handleLogout(request):
+    return render(request,"login.html")
