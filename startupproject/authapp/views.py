6568b4cceeaeb3430351d4ba78921db42da8a568
anees <aneesurrehman423@gmail.com>
2023-06-09 08:45:51 +0530

    day-9

--- startupproject/authapp/views.py
+++ startupproject/authapp/views.py
@@ -62,8 +62,8 @@ def signup(request):
 
             })
 
-            # email_message = EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email])
-            # email_message.send()
+            email_message = EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email])
+            email_message.send()
             messages.success(request,f"Activate Your Account by clicking the link in your gmail {message}")
             # messages.success(request,f"{message}")
             return redirect('/auth/login/') 
@@ -115,3 +115,98 @@ def handleLogout(request):
     logout(request)
     messages.success(request,"Logout success")
     return render(request,"login.html")
+
+
+
+class RequestResetEmailView(View):
+    def get(self,request):
+        return render(request,'request-reset-email.html')
+
+    def post(self,request):
+
+        email=request.POST['email']
+        user=User.objects.filter(email=email)
+
+        if user.exists():
+            # current_site=get_current_site(request)
+            email_subject='[Reset Your Password]'
+            message=render_to_string('reset-user-password.html',{
+                'domain':'127.0.0.1:8000',
+                'uid':urlsafe_base64_encode(force_bytes(user[0].pk)),
+                'token':PasswordResetTokenGenerator().make_token(user[0])
+            })
+
+            email_message=EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email])
+            email_message.send()
+            messages.info(request,f"{message} " )
+            return render(request,'request-reset-email.html')
+        else:
+            messages.error(request,'No Account Exists with this email' )
+            return render(request,'request-reset-email.html')    
+
+
+
+class SetNewPasswordView(View):
+    def get(self,request,uidb64,token):
+        context = {
+            'uidb64':uidb64,
+            'token':token
+        }
+        try:
+            user_id=force_text(urlsafe_base64_decode(uidb64))
+            user=User.objects.get(pk=user_id)
+
+            if  not PasswordResetTokenGenerator().check_token(user,token):
+                messages.warning(request,"Password Reset Link is Invalid")
+                return render(request,'request-reset-email.html')
+
+        except DjangoUnicodeDecodeError as identifier:
+            pass
+
+        return render(request,'set-new-password.html',context)
+
+    def post(self,request,uidb64,token):
+        context={
+            'uidb64':uidb64,
+            'token':token
+        }
+        flag = 0
+        password=request.POST['pass1']
+        confirm_password=request.POST['pass2']
+        if password!=confirm_password:
+            messages.warning(request,"Password is Not Matching")
+            return render(request,'set-new-password.html',context)
+        
+        if len(password)<=8:
+            messages.warning(request,"Password must be atleast 8 character")
+            return render(request,'set-new-password.html',context)
+        elif not re.search("[a-z]", password):
+            flag = -1
+            
+        elif not re.search("[A-Z]", password):
+            flag = -1
+            
+        elif not re.search("[0-9]", password):
+            flag = -1
+            
+        elif not re.search("[_@$]" , password):
+            flag = -1  
+        else:
+            pass
+
+
+       
+        if(flag==0):  
+            try:
+                user_id=force_text(urlsafe_base64_decode(uidb64))
+                user=User.objects.get(pk=user_id)
+                user.set_password(password)
+                user.save()
+                messages.success(request,"Password Reset Success Please Login with NewPassword")
+                return redirect('/')
+
+            except DjangoUnicodeDecodeError as identifier:
+                messages.error(request,"Something Went Wrong")
+                return render(request,'set-new-password.html',context)
+
+  
